6ad9a932a732645007d16bad26686e35415345c5
Gabiroba <gabrielcardosogirarde@gmail.com>
2023-12-27 14:13:48 -0300

    Start

--- componentes/src/app/app.component.ts
+++ componentes/src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
+  imports: [CommonModule],
+  standalone: true,
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'componentes';
+
+
 }
